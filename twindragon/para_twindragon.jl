c88078ea5a5eb74364a9c21993d0e6fd634ce038
Štěpán Starosta <staroste@fit.cvut.cz>
2015-12-14 22:46:15 +0100

    parallel twindragon tweaks, episode 1

--- twindragon/para_twindragon.jl
+++ twindragon/para_twindragon.jl
@@ -4,31 +4,34 @@ using Images
 
 @everywhere include("Twindragon.jl")
 
-@everywhere xmin=-3.
-@everywhere xmax=3.
-@everywhere ymin=-3.
-@everywhere ymax=3.
+@everywhere xmin=-1.7
+@everywhere xmax=1.4
+@everywhere ymin=-1.3
+@everywhere ymax=2.2
 @everywhere dx=0.01
 @everywhere dy=0.01
 @everywhere array_length=40
-  
+
 @everywhere dimx = floor(Int64, (xmax - xmin) / dx)
 @everywhere dimy = floor(Int64, (ymax - ymin) / dy)
 
 image = SharedArray(Float64, dimx, dimy)
-  
+
 @everywhere oldset = SimpleSet(array_length)
 @everywhere newset = SimpleSet(array_length)
 
+
 function run!()
-  @parallel for jx in 1:dimx
-    for jy in 1:dimy
-      image[jx, jy] = twindragon([xmin + (jx - 0.5)*dx, ymin + (jy - 0.5)*dy], oldset, newset, ball)
+  @sync @parallel for jx = 1:dimx for jy = 1:dimy
+      @inbounds image[jx, jy] = twindragon([xmin + (jx - 0.5)*dx, ymin + (jy - 0.5)*dy], oldset, newset, ball)
     end
   end
-
   return image
 end
 
 
-save("twindragon.png", grayim(run!()))
+println("Starting...")
+
+tic()
+save("twindragon_para.png", grayim(run!()))
+toc()
